a3158b074c27a44e62921b9e77be3238d9af5852
Bonaspuram Divesh Reddy <diveshreddy2427@gmail.com>
2022-10-18 16:50:23 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<pre><?php
+<?php
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+$username = $_POST['username'];
+$password = $_POST['password'];
+require_once('dbConnect.php');
+$sql= "SELECT * FROM user WHERE username = '$username' AND password = '$password' ";
+$result = mysqli_query($con,$sql);
+$check = mysqli_fetch_array($result);
+if(isset($check)){
+echo 'success';
+}else{
+echo 'failure';
+}
+}
+?>
